adb6663fa239e84f8f180e995bd16dc5c45f6a28
tw-transactworld <haresh.panchal@paymentz.com>
2017-12-12 15:14:40 +0530

    Files Deleted

--- includes/languages/english/modules/payment/paymentz.php
+++
@@ -1,9 +0,0 @@
-<?php
-  define('MODULE_PAYMENT_PAYMENTZ_TEXT_TITLE', 'TransactWorld');
-  define('MODULE_PAYMENT_PAYMENTZ_TEXT_DESCRIPTION', 'Please make your check or money order payable to:<br />' . MODULE_PAYMENT_TRANSACTWORLD_MERCHANT_ID . '<br />');
-  define('MODULE_PAYMENT_PAYMENTZ_TEXT_EMAIL_FOOTER', "Please make your check or money order payable to:" . "\n\n" . MODULE_PAYMENT_TRANSACTWORLD_MERCHANT_ID . "\n");\define('MODULE_PAYMENT_PAYMENTZ_TEXT_DESCRIPTION', ' Enable TransactWorld Online Payments');
-define('MODULE_PAYMENT_PAYMENTZ_TEXT_ERROR', 'Credit Card Error!');
- define('MODULE_PAYMENT_PAYMENTZ_TEXT_ERROR_MESSAGE', 'There has been an error processing you credit card at TransactWorld, please try again.');
-define('MODULE_PAYMENT_PAYMENTZ_ALERT_ERROR_MESSAGE', 'Security Error. Illegal access detected, please try again.');
-
-?>
